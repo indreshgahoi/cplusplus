c08b5f8a35c9ebcc92b431c2dd15bc082d6453be
indreshgahoi <indreshgahoi@gmail.com>
2013-09-20 10:23:58 +0530

    Update ConvexPolyGun.c
    update polar angle code

--- src/spoj/Geometry/SweepLine/ConvexPolyGun.c
+++ src/spoj/Geometry/SweepLine/ConvexPolyGun.c
@@ -16,16 +16,32 @@ struct Point
 
 typedef struct Point Point;
 
-/* polar angle of point p w.r.t to orgin (0,0)
- *  it returns Angle in range [0,2*pi)
- *  If Point is (0,0) return -1
- */
-double polarAngle(Point p)
+
+double polarAngle(const point &p)
 {
-	if(fabs(p.x)<=EPS && fabs(p.y)<=EPS) return -1;
-	if(fabs(p.x)<=EPS) return (p.y>EPS? 1:3)*acos(0);
-	double theta=atan(1.0*p.y/p.x);
-	if(fabs(p.x)>EPS) return (p.y>-EPS?theta:(4*acos(0)+theta));
-	else return 2*acos(0)+theta;
+    if(fabs(p.x)<=EPS && fabs(p.y)<=EPS) return -1.0;
+    if(fabs(p.x)<=EPS) return (p.y>EPS? 1.0:3.0) *acos(0);
+    double theta=atan(p.y/p.x);
+    
+    if(p.x>EPS) return (p.y>=-EPS)?theta:(4*acos(0)+theta);
+    return (2*acos(0)+theta);
 }
 
+/* To detect the the point is either clock wise (right turn) or
+* counter clock wise (left turn) or colinear
+* retrun 1 counter clock wise
+* return 2 clock wise
+* return 0 colinear
+*/
+int ccw(const point &p,const point &q,const point &r)
+{
+    point a={p.x-r.x,p.y-r.y};
+    point b={q.x-r.x,q.y-r.y};
+    // a=ax i+ ay j
+    // b=bx i + by j
+    // a*b= ax*by - ay*bx cross product
+    double cp= a.x*b.y-a.y*b.x;
+    if(cp>0) return 1;
+    if(cp<0) return 2;
+    else return 0;
+}
