b009ca1c93fe2fd9de426273d7310402bd2959ef
indreshgahoi <indreshgahoi@gmail.com>
2013-09-27 17:12:40 +0530

    Update RangeSearch.cpp
    add lower_bound and and upper_bound method

--- src/InterviewPreparation/binarySearch/RangeSearch.cpp
+++ src/InterviewPreparation/binarySearch/RangeSearch.cpp
@@ -6,6 +6,72 @@
  */
 
 #include "constant.h"
+//
+//  lower.c
+//  practice_problem
+//
+//  Created by Indresh on 9/26/13.
+//
+//
+
+#include <stdio.h>
+
+
+#define __advanse(ptr,size,step)    (ptr=(char*)ptr+(size*step))
+
+void* lower_bound(void *first ,size_t len, size_t elem_size, void *value, int (*cmp) (const void *,const void *)){
+   
+    size_t len1;
+    void *m ;
+    while(len!=0){
+            len1=len>>1;
+            m=first;
+            __advanse(m,elem_size,len1);
+            if(cmp(m,value)){
+                m=(char*)m+elem_size;
+                first=m;
+                len-=len1+1;
+            }
+            else
+                len = len1;
+    }
+    return first;
+}
+
+void* upper_bound(void *first, size_t len ,size_t elem_size , void *value ,int (*cmp) (const void * , const void *)){
+   
+    size_t len1 ;
+    void *m ;
+    
+    while(len!=0){
+        len1=len>>1;
+         m=first;
+        __advanse(m, elem_size, len1);
+        if(cmp(value,m)){
+            len=len1;
+        }
+        else{
+            first=m=(char*)m+elem_size;
+            len-=len1+1;
+        }
+        
+    }
+    return first;
+}
+
+int compare(const void *a , const void *b){
+    return *((int *)a)<*((int *)b);
+}
+
+#define ARRAY_SIZE(A) (sizeof(A)/sizeof(A[0]))
+
+void test_lower_bound(){
+    int a[]={1,1,2,2,3,3,4,4,5,5,5,5,6,7,8,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,10,11,11,11,13,13,17};
+    int i = 13;
+   // int *b=(int *)lower_bound(a,ARRAY_SIZE(a),4,&i,&compare);
+    int *b=(int *)upper_bound(a,ARRAY_SIZE(a),4,&i,&compare);
+        printf("value :%d  \n",*(b));
+}
 
  vector<int> searchRange(int A[], int n, int target) {
      vector<int> ans;
